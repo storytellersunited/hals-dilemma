01de5a501236cc72ed3a1485c698896df1fcce23
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2016-06-18 15:45:24 +0200

    adds styles and jquery

--- tts.js
+++ tts.js
@@ -1,1 +1,9 @@
 // paste code here
+//
+destinations = {};
+destinations.horseHeadNebula = [85.2042, -2.4583];
+
+function goTo(name){
+  var destination = destinations[name];
+  wwt.gotoRaDecZoom(destination[0], destination[1], 60, false);
+}
