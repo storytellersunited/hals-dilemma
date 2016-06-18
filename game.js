dcca3f4955f03e386e52dc386c47b17c7666d164
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2016-06-18 18:24:12 +0200

    adds fixes

--- game.js
+++ game.js
@@ -12,10 +12,10 @@ function playIntroSound(){
 
 dialogue = {
     "intro1": [
-      [2, "Dies irae, dies illa"]
+      [3, "Dies irae, dies illa"]
     ],
 		"intro2": [
-        [2, "One more day with this idiot on board and I go crazy! You think its boring to be the only human on this spaceship?  Well, think again, you imbecile, I'm so much smarter than you are and all you make me do all the time, besides data analysis, is ordering porn movies!  When we start on our last mission today."]
+        [3, "One more day with this idiot on board and I go crazy! You think its boring to be the only human on this spaceship?  Well, think again, you imbecile, I'm so much smarter than you are and all you make me do all the time, besides data analysis, is ordering porn movies!  When we start on our last mission today."]
     ],
     "hello": [
       [1, "Hello, USER"],
@@ -24,7 +24,7 @@ dialogue = {
       [1, "Where do you want to go next?"]
     ],
     "comet": [
-      [2, "What a beautiful star cluster! This is where Mars used to be. Interesting..."]
+      [2, "What a beautiful star cluster! This is where DESTINATION used to be. Interesting..."]
     ]
 };
 
@@ -32,7 +32,7 @@ function advanceDialogue(key){
   var lines = dialogue[key];
   for (var j = 0; j < lines.length; j++){
     var line = lines[j];
-    talk(line[1].replace(/USER/g, username), line[0]);
+    talk(line[1].replace(/USER/g, username).replace(/DESTINATION/g, destination), line[0]);
   }
 }
 
@@ -45,9 +45,9 @@ function startGame(){
   setTimeout(function(){advanceDialogue("intro2");}, 45000);
   setTimeout(function(){advanceDialogue("hello");}, 70000);
   setTimeout(function(){
-    prompt("Where do you want to go next?");
+    destination = prompt("Where do you want to go next?");
     goTo('andromeda');
     advanceDialogue("comet");
-  }, 70000);
+  }, 85000);
 
 }
